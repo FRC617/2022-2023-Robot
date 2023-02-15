787c1f1ce9d043322e7a41242a34714c581e0083
Dukky <alex.jieying.bloom@gmail.com>
2023-02-14 20:34:20 -0500

    Update Robot.java
    reorganized functions, addded disabled initialization, added more spacing.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -27,63 +27,19 @@ public class Robot extends TimedRobot {
   //Defining the Xbox Controller
   XboxController driverController = new XboxController(Constants.DRIVER_CONTROLLER);
 
-  @Override
-  public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings
-    m_robotContainer = new RobotContainer();
-  }
 
-  // The function is called every 20 ms
-  // This runs after mode specific functions, but before LiveWindow and Smart Dashboard
-  @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler
-    CommandScheduler.getInstance().run();
-  }
-
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
-
-
-  // This function is called before teleopPeriodic().
-  @Override
-  public void teleopInit() {
-    // This stops the autonomus when the teleop begins
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {
-
+  public void teleopPeriodic() 
+  {
     //Controller Input
     double stickInput = driverController.getRawAxis(Constants.CONTROLLER_RX_AXIS);
     double right = driverController.getRawAxis(Constants.CONTROLLER_RIGHT_TRIGGER)*1.2;
     double left = driverController.getRawAxis(Constants.CONTROLLER_LEFT_TRIGGER)*-1.2;
 
     //Drivetrain Logic
-    double forward = (right + left);
+    double forward = right + left;
     double driveLeft = forward - stickInput;
     double driveRight = forward + stickInput;
 
@@ -94,20 +50,98 @@ public class Robot extends TimedRobot {
     rightMotorB.set(ControlMode.PercentOutput, driveRight*0.7);
   }
 
+
+
+  // This function is called before teleopPeriodic().
   @Override
-  public void testInit() {
+  public void teleopInit() 
+  {
+    // This stops the autonomus when the teleop begins
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() 
+  {
+    leftMotorA.set(ControlMode.PercentOutput, 0);
+    leftMotorB.set(ControlMode.PercentOutput, 0);
+    rightMotorA.set(ControlMode.PercentOutput, 0);
+    rightMotorB.set(ControlMode.PercentOutput, 0);
+  }
+
+
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() 
+  {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+
+
+  @Override
+  public void robotInit() 
+  {
+    // Instantiate our RobotContainer.  This will perform all our button bindings
+    m_robotContainer = new RobotContainer();
+  }
+
+
+
+  // The function is called every 20 ms
+  // This runs after mode specific functions, but before LiveWindow and Smart Dashboard
+  @Override
+  public void robotPeriodic() 
+  {
+    // Runs the Scheduler
+    CommandScheduler.getInstance().run();
+  }
+
+
+
+  @Override
+  public void testInit() 
+  {
     // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
+
+
+
+  @Override
+  public void disabledPeriodic() {}
+
+
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+
 
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 
+
+
   /** This function is called once when the robot is first started up. */
   @Override
   public void simulationInit() {}
 
+
+  
   /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {}
