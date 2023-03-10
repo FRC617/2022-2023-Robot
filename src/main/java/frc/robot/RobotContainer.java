35f73f76728d895c3c8418a66f888b0f0cb6f175
Dukky <alex.jieying.bloom@gmail.com>
2023-03-09 22:42:47 -0500

    Timed Setup and additive gear shifting
    yay

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,63 +1,136 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+    VictorSPX leftMotorFront = new VictorSPX(Constants.LEFT_MOTOR_A * -1);
+    VictorSPX leftMotorBack = new VictorSPX(Constants.LEFT_MOTOR_B * -1);
+    VictorSPX rightMotorFront = new VictorSPX(Constants.RIGHT_MOTOR_A);
+    VictorSPX rightMotorBack = new VictorSPX(Constants.RIGHT_MOTOR_B);
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+    double stickInput;
+    double right;
+    double left;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-  }
+    double prevXAccel = 0;
+    double prevYAccel = 0;
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    boolean buttonA;
+    boolean buttonB;
+    boolean buttonX;
+    boolean buttonY;
+    
+    Accelerometer accelerometer = new BuiltInAccelerometer();
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-  }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
-  }
+    //Gets Controller Inputs
+    public void controllerInput() {
+        XboxController driverController = new XboxController(Constants.DRIVER_CONTROLLER);
+    
+        stickInput = driverController.getRawAxis(Constants.CONTROLLER_RX_AXIS);
+        right = driverController.getRawAxis(Constants.CONTROLLER_RIGHT_TRIGGER)*1.2;
+        left = driverController.getRawAxis(Constants.CONTROLLER_LEFT_TRIGGER)*-1.2;
+
+        buttonA = driverController.getAButton();
+        buttonB = driverController.getBButton();
+        buttonX = driverController.getXButton();
+        buttonY = driverController.getYButton();
+    }
+
+
+    public void driveTrainLogic() {
+        //Drivetrain Logic
+        double forward = right + left;
+        double driveLeft = forward - stickInput;
+        double driveRight = forward + stickInput;
+
+        int buttonLevel = 0;
+
+        //Count button levels up
+        if (buttonA == true) {
+            buttonLevel += 1;
+        }
+
+        //Reset button level to 0 after 4 presses.
+        if (buttonLevel >= 4) {
+            buttonLevel = 0;
+        }
+    
+        //Sending Power to Motors at Different Strengths
+        switch (buttonLevel) {
+            case 0 :
+                leftMotorFront.set(ControlMode.PercentOutput, driveLeft*0.7);
+                leftMotorBack.set(ControlMode.PercentOutput, driveLeft*0.7);
+                rightMotorFront.set(ControlMode.PercentOutput, driveRight*0.7);
+                rightMotorBack.set(ControlMode.PercentOutput, driveRight*0.7);
+                break;
+            case 1 :
+                leftMotorFront.set(ControlMode.PercentOutput, driveLeft*0.3);
+                leftMotorBack.set(ControlMode.PercentOutput, driveLeft*0.3);
+                rightMotorFront.set(ControlMode.PercentOutput, driveRight*0.3);
+                rightMotorBack.set(ControlMode.PercentOutput, driveRight*0.3);
+                break;
+            case 2 :
+                leftMotorFront.set(ControlMode.PercentOutput, driveLeft*0);
+                leftMotorBack.set(ControlMode.PercentOutput, driveLeft*0);
+                rightMotorFront.set(ControlMode.PercentOutput, driveRight*0);
+                rightMotorBack.set(ControlMode.PercentOutput, driveRight*0);
+                break;
+            case 3 :
+                leftMotorFront.set(ControlMode.PercentOutput, driveLeft*1);
+                leftMotorBack.set(ControlMode.PercentOutput, driveLeft*1);
+                rightMotorFront.set(ControlMode.PercentOutput, driveRight*1);
+                rightMotorBack.set(ControlMode.PercentOutput, driveRight*1);
+                break;
+        }
+    }
+
+    //Turns Motor Power to 0
+    public void motorsDisabled() {
+        leftMotorFront.set(ControlMode.PercentOutput, 0);
+        leftMotorBack.set(ControlMode.PercentOutput, 0);
+        rightMotorFront.set(ControlMode.PercentOutput, 0);
+        rightMotorBack.set(ControlMode.PercentOutput, 0);
+    }
+
+    //Sends Motor Data to Dashboard
+    public void motors() {
+        double motorPowerLF = leftMotorFront.getBusVoltage();
+        double motorPowerLB = leftMotorBack.getBusVoltage();
+        double motorPowerRF = rightMotorFront.getBusVoltage();
+        double motorPowerRB = rightMotorBack.getBusVoltage();
+    
+        shuffleBoardDisplay(motorPowerLF, "Front Left Motor");
+        shuffleBoardDisplay(motorPowerLB, "Front Right Motor");
+        shuffleBoardDisplay(motorPowerRF, "Back Left Motor");
+        shuffleBoardDisplay(motorPowerRB, "Back Right Motor");
+    }
+
+
+    public void acceleration() {
+        double xAccel = accelerometer.getX();
+        double yAccel = accelerometer.getY();
+    
+        // Calculates the jerk in the X and Y directions
+        // Divides by .02 because default loop timing is 20ms
+        double xJerk = (xAccel - prevXAccel)/.02;
+        double yJerk = (yAccel - prevYAccel)/.02;
+    
+        prevXAccel = xAccel;
+        prevYAccel = yAccel;
+    
+        shuffleBoardDisplay(xJerk, "x Jerk");
+        shuffleBoardDisplay(yJerk, "y Jerk");
+    }
+
+
+    //Function to Display Data on ShuffleBoard 
+    public void shuffleBoardDisplay(double shuffleBoardInput, String inputName) {
+        SmartDashboard.putNumber(inputName, shuffleBoardInput);
+    }
 }
